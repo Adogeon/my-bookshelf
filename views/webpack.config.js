97380e837359477aa72163828e61c1537c4ced5b
Anh <addao823@gmail.com>
2021-06-29 13:52:11 -0700

    replace react-hot-loader with react-refresh

--- views/webpack.config.js
+++ views/webpack.config.js
@@ -1,8 +1,12 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
+  mode: isDevelopment ? "development" : "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -18,6 +22,7 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
