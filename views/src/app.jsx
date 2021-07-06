f73da401a4857154299a2af2244e4bc72a0727e9
Anh <addao823@gmail.com>
2021-07-06 11:15:11 -0700

    Adding container to main

--- views/src/app.jsx
+++ views/src/app.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import NavBar from "./layouts/NavBar";
 import RouterView from "./layouts/RouterView";
 
+import layout from "./styles/row.css";
+
 const App = () => {
   return (
     <Router>
       <NavBar />
-      <main>
+      <main className={`${layout["col-8"]} ${layout['m-auto']}`}>
         <RouterView />
       </main>
     </Router>
