6896d29020c2f147d342b99e26592f9c325aca73
Anh <addao823@gmail.com>
2021-07-07 16:55:42 -0700

    Refactor new Challenge page

--- views/src/pages/NewChallenge.jsx
+++ views/src/pages/NewChallenge.jsx
@@ -1,48 +1,7 @@
 import React, { useState } from "react";
 // Page for add new challenge
 
-// props => JSX
-// return an input field with validation rule
-// example: if props.required  true, value should not be empty
-// props
-// - required: boolean value to set if this is required
-// - error: error received from the submit validate step
-const InputField = ({ required, ...props }) => {
-  const [value, setValue] = useState(null);
-  const [error, setError] = useState(null);
 
-  const validateValue = () => {
-    if (required && value.trim === "") {
-      setError("This field is empty!");
-    }
-  };
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-    validateValue();
-  };
-
-  return (
-    <>
-      <label
-        className={required ? `option-input` : `required-input`}
-        {...props}
-        for={props.title}
-      >
-        {props.title}
-      </label>
-      <input
-        name={props.title}
-        className={
-          value !== null ? "input" : error ? `error-input` : `correct-input`
-        }
-        type="text"
-        value={value}
-        onChange={handleChange}
-      />
-    </>
-  );
-};
 
 // void -> JSX
 // return a JSX object for NewChallenge Page
@@ -57,7 +16,58 @@ const InputField = ({ required, ...props }) => {
 // - validation of input
 // - validation error
 const NewChallenge = () => {
-  return <>This is the page for new challenge</>;
-};
+  const [state, setState] = useState({
+    name: '',
+    description: '',
+    error: {
+      name: "",
+    },
+    nameValid: false,
+    formValid: false
+  })
+
+  const validateForm = () => {
+    setState(prevState => ({formValid: prevState.nameValid,...prevState}))
+  }
+
+  const validateField = (fieldName, value) => {
+    let error = state.error;
+    let nameValid = state.nameValid;
+    switch(fieldName) {
+      case 'name':
+        nameValid = nameValid.trim !== "";
+        error.name = nameValid ? '' : "Challenge name should not be empty";
+    }
+
+    setState(prevState => ({error, nameValid, ...prevState}))
+    validateForm()
+  }
+
+  const handleUserInput = (e) => {
+    const name = e.target.name;
+    const value = e.taregt.value;
+    if(name = 'name') {
+      validateField(name, value)
+    }
+    setState(prevState => ({[name]: value, ...prevState}))
+  }
+
+  return (
+  <>
+    <h1> Add Your Own Challenge</h1>
+    <div>
+      <label htmlFor="name">Challenge Name*</label>
+      <input type="text" name="name" value={state.name} onChange={(event) => handleUserInput(event)} />
+      {error.name !== "" ?? <span>{error.name}</span>}
+    </div>
+    <div>
+      <label htmlFor="desc">Description</label>
+      <input type="text" name="desc" value={state.description} placeholder="A quick summary of your challenge" />
+    </div>
+    <button type="submit" disabled={!state.formValid}>
+      Next
+    </button>
+  </>
+}
 
 export default NewChallenge;
